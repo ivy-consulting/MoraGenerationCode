5bf2bdc27320a92d26ecfd97658aacb5a8ac3bc9
andromedalactea <bry3639@gmil.com>
2024-02-07 14:25:55 -0500

    [FEAT] audio analytics ignore the puntuation symbols for the time distribution

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,3 @@
-import whisper_timestamped
-import json
-import os
-
 def distribute_time_equally(start, end, text, decimals=4):
     """
     Distributes the time among the symbols of a word, excluding specific symbols.
@@ -58,56 +54,11 @@ def distribute_time_equally(start, end, text, decimals=4):
     
     return symbols_times
 
-def add_consonant_vowel_info(symbols, mapping_file):
-    """
-    Adds consonant and vowel information to each symbol.
-    """
-    if not os.path.exists(mapping_file):
-        print(f"Mapping file {mapping_file} does not exist. Adding null for consonant and vowel.")
-        for symbol in symbols:
-            symbol["consonant"] = None
-            symbol["vowel"] = None
-        return symbols
-
-    with open(mapping_file, 'r', encoding='utf-8') as file:
-        mapping = json.load(file)
-
-    for symbol in symbols:
-        char_info = mapping.get(symbol["symbol"], {})
-        symbol["consonant"] = char_info.get("consonant", None)
-        symbol["vowel"] = char_info.get("vowel", None)
-    
-    return symbols
-
-def audio_query_json(audio_path, save_to_file=False, json_output_path="speech_symbol_timestamps.json", mapping_file="files/mapping.json"):
-    """
-    Transcribes an audio file and adds consonant and vowel information to each symbol.
-    """
-    model = whisper_timestamped.load_model("base", device="cpu")
-    result = whisper_timestamped.transcribe(model, audio_path, language="ja")
-    print(result)
-    print("Complete transcription:", result["text"])
-
-    data_to_save = {
-        "transcription": result["text"],
-        "symbols": []
-    }
-
-    for segment in result["segments"]:
-        for word in segment.get("words", []):
-            symbols_times = distribute_time_equally(word['start'], word['end'], word['text'])
-            data_to_save["symbols"].extend(symbols_times)
-
-    # Add consonant and vowel information
-    data_to_save["symbols"] = add_consonant_vowel_info(data_to_save["symbols"], mapping_file)
-
-    if save_to_file:
-        with open(json_output_path, 'w', encoding='utf-8') as json_file:
-            json.dump(data_to_save, json_file, ensure_ascii=False, indent=4)
-        print(f"Results saved in: {json_output_path}")
-    
-    return data_to_save
-
-# Example of use
-audio_path = "test_audios/japanesef32.wav"
-transcription_json = audio_query_json(audio_path, save_to_file=True)
+# Example usage
+start_time = 0
+end_time = 10
+text = "ットはありますか?"
+# Print the result to see how time is distributed
+symbols_times = distribute_time_equally(start_time, end_time, text)
+for symbol_time in symbols_times:
+    print(symbol_time)
